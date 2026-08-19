9c94456c4ff414dad0792941adc048b0dcfee1ea
Arpan Verma <arpan24622@gmail.com>
2026-08-19 10:28:27 +0530

    remove lifecycle rule from bootstrap\main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -43,16 +43,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    id     = "expire-old-versions"
-    status = "Enabled"
-
-    noncurrent_version_expiration {
-      noncurrent_days = 90
-    }
-  }
-}
